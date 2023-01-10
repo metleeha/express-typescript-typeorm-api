50749b96b36028295694e2e7592884c1ed775bae
Hadong Lee <hadongme@gmail.com>
2023-01-11 02:20:07 +0900

    add express setup & configure db connection with typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-const world = 'Hi';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Application } from './app';
+import * as dotenv from 'dotenv';
 
-export function hello(word: string = world): string {
-  return `Hello ${world}! `;
-}
+dotenv.config();
+
+const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || ''),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DBNAME,
+    entities: ["src/entities/**/*.ts"],
+    synchronize: true,
+})
+
+AppDataSource.initialize()
+    .then(async () => {
+        console.log("Data Source has been initialized!")
+        const application: Application = new Application();
+        application.startServer();
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
